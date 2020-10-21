dc0ec9ed358efc7e9bf18daa702d59aca776390e
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2020-10-21 17:32:55 -0400

    added time columns...that I copied from Shannon in our 5pm study grou

--- logic.js
+++ logic.js
@@ -1,13 +1,3 @@
-const today = new Date();
-
-let month = today.getMonth()+1;
-let day = today.getDate();
-
-let output = today.getFullYear() + '/' +
-    (month<10 ? '0' : '') + month + '/' +
-    (day<10 ? '0' : '') + day;
-    document.write(output);
-
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
@@ -21,13 +11,77 @@ let output = today.getFullYear() + '/' +
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
-
+$(document).ready(function () {
+    //your code here
+  });  
 
 //STEP: Create a timer with date using js
 
+let today = new Date();
+// const currentTime = new Date();
+// const time = today.getHours() + ":" + today.getMinutes();
+
+let minute = today.getMinutes();
+let hour = today.getHours();
+let month = today.getMonth()+1;
+let day = today.getDate();
+let year = today.getFullYear();
+let container = $(".container")
+
+
+let date = 
+    (month<10 ? '0' : '') + month + '/' + (day<10 ? '0' : '') + day + '/' + year;
+    document.getElementById("today").textContent=date;
+
+let time = hour + ":" + minute;
+    document.getElementById("time").textContent=time;
 //STEP: create time blocks 
+//THE ARRAY AND FOR LOOP IS TAKEN FROM SHANNON'S VIA 5PM STUDY GROUP
+let hours = ["8:00am", "9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "500pm"];
+
+for (var i = 0; i < hours.length; i++) {
+    //Create row for each hour of the day
+    var hourRow = $("<div>");
+    //Assign row class
+    hourRow.attr("class", "row");
+    //Create column for the hour to display in
+    var hourCol = $("<div>");
+    //Assign column class
+    hourCol.attr("class", "col-2");
+    //Append column to row
+    hourRow.append(hourCol);
+    //Set text of column to hour of the day
+    hourCol.text(hours[i]);
+    //Append the row to the container
+    container.append(hourRow);
+    //Create column for user input, the plan for that hour of the day
+    var planCol = $("<div>");
+    //Assign class to middle column
+    planCol.attr("class", "col-9");
+    //Just to test
+    planCol.text("Test");
+    //Append plan column to the row (which is already appended to container)
+    hourRow.append(planCol);
+    //Create column for save button
+    var saveBtnCol = $("<div>");
+    //Assign class to last column of row
+    saveBtnCol.attr("class", "col-1");
+    //Append column to row
+    hourRow.append(saveBtnCol);
+    //Create save button
+    var saveBtn = $("<button>");
+    //Assign attributes to button to apply CSS
+    saveBtn.attr("type", "button");
+    saveBtn.attr("class", "saveBtn btn btn-success");
+    //Set text content of save button
+    saveBtn.text("Save");
+    //Append save button to last column (which is already appended to the row)
+    saveBtnCol.append(saveBtn);
+}; 
+
 
 //STEP: color code time blocks
+
 
 //STEP: make them click responsive (change color when clicked?!)
 
