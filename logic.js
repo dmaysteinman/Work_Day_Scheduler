b4de9c385ddc0e41aed9bd7008239d1d54eabbab
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2020-10-24 19:12:07 -0400

    worked with my tutor

--- logic.js
+++ logic.js
@@ -1,3 +1,4 @@
+
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar 
@@ -26,12 +27,30 @@ currentDay.text(m.format("[Today is] MMM Do[,] YYYY"));
 var currentTime = $("#currentTime");
 currentTime.text(m.format("[The time is] h:mm:ss a"));
 var update = function() {
+    let timeNow = moment().format("H");
+    console.log(timeNow);
     document.getElementById("currentTime")
     .innerHTML = moment().format("[The time is] h:mm:ss a");
-}
+    for (let i=8; i<=18; i++) {
+      if (i < timeNow) {
+        $(`#${i}`).removeClass("blue");
+        $(`#${i}`).removeClass("green");
+        $(`#${i}`).addClass("red");
+      } else if (i=timeNow) {
+        $(`#${i}`).removeClass("red");
+        $(`#${i}`).removeClass("green");
+        $(`#${i}`).addClass("blue");
+      } else {
+        $(`#${i}`).removeClass("red");
+        $(`#${i}`).removeClass("blue");
+        $(`#${i}`).addClass("green");
+      }
+    }
+};
 setInterval(update, 1000);
 
-var container = $(".container")
+var container = $(".container");
+var myWorkDay = $("#timeblocks");
 
 
 //STEP: create time blocks 
@@ -40,19 +59,45 @@ var container = $(".container")
 
 
 //STEP: color code time blocks
-
+ 
 
 //STEP: make them click responsive (change color when clicked?!)
 
 //STEP: make a save button for time blocks that saves in local storage
-var saveBtn = $("<button>")
-saveBtn.attr("type", "button");
-saveBtn.attr("class", "saveBtn");
-saveBtn.text("Save");
-$("#save").append(saveBtn);
+//IN HTML
+// var names = [];
+// names[0] = prompt("New member name?");
+// localStorage.setItem("names", JSON.stringify(names));
+
+//...
+
+var schedule = [];
+
+// var storedNames = JSON.parse(localStorage.getItem("names"));
+
+$(".saveBtn").click(function(event){
+  console.log("id = ",event.currentTarget.id);
+  let content = $(event.currentTarget).parent().parent().children()[1].innerHTML;
+  console.log(content)
+  let index = parseInt(event.currentTarget.id)-8;
+  console.log(index);
+  schedule[index]=content;
+  localStorage.setItem("schedule",JSON.stringify(schedule));
+
+  });
+
+  
+  //STEP: make the saved events stay when the page is refreshed
 
 
-
-
-
-//STEP: make the saved events stay when the page is refreshed
+function init() {
+  for (let i=0; i<=10; i++){
+    schedule.push("");
+  }
+  console.log("Local storage get item"+localStorage.getItem("schedule"));
+  schedule = JSON.parse(localStorage.getItem("schedule"))
+  console.log("schedule = " + schedule)
+  for (let i=8; i<=18; i++) {
+    
+  }
+$(init)
